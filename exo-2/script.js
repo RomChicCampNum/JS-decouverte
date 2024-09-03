4e1422cc47ab61c7beaf7d2d340f869febf89d4d
Romain CHICA <romain.chica@Digital-Grenoble.local>
2024-09-03 10:26:49 +0200

    Exercice 2

--- exo-2/script.js
+++ exo-2/script.js
@@ -1,2 +1,39 @@
-// you can write js here
-console.log('exo-2');
+const myDate = new Date();
+const weekdayMessage = "Pas encore, c'est un jour de semaine.";
+const weekendMessage = "C'est le weekend, profitez-en !";
+const fridayEveningMessage = "C'est presque le weekend, tenez bon !";
+const mondayMorningMessage = "C'est encore le weekend, dormez un peu plus.";
+
+const currentDay = myDate.getDay();
+const currentHour = myDate.getHours();
+
+let message;
+
+const isTesting = true;
+
+if (isTesting) {
+    const testDay = parseInt(prompt("Entrez un jour (0 = dimanche, 1 = lundi, ..., 6 = samedi) :"));
+    const testHour = parseInt(prompt("Entrez une heure (0-23) :"));
+
+    if (testDay === 0 || testDay === 6) {
+        message = weekendMessage;
+    } else if (testDay === 5 && testHour >= 17) {
+        message = fridayEveningMessage;
+    } else if (testDay === 1 && testHour < 9) {
+        message = mondayMorningMessage;
+    } else {
+        message = weekdayMessage;
+    }
+} else {
+    if (currentDay === 0 || currentDay === 6) {
+        message = weekendMessage;
+    } else if (currentDay === 5 && currentHour >= 17) {
+        message = fridayEveningMessage;
+    } else if (currentDay === 1 && currentHour < 9) {
+        message = mondayMorningMessage;
+    } else {
+        message = weekdayMessage;
+    }
+}
+
+console.log(message);
