bab258610f35ce49de85136905bf599b40ba4e9f
Romain <romain.chica@le-campus-numerique.com>
2024-09-04 15:52:56 +0200

    Exercice 5

--- exo-5/exo5.js
+++ exo-5/exo5.js
@@ -1,3 +1,16 @@
 // you can write js here
 
-console.log('exo-5');
+let input = "Turpentine and turtles";
+const vowels = ['a', 'e', 'i', 'o', 'u'];
+let resultArray = [];
+
+for (let i = 0; i < input.length; i++) {
+    for (let j = 0; j < vowels.length; j++) {
+        if (input[i].toLowerCase() === vowels[j]) {
+            resultArray.push(input[i]);
+        }
+    }
+}
+
+let whaleTranslation = resultArray.join('').toUpperCase();
+console.log(whaleTranslation); // Affiche: UEIEAUE
