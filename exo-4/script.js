227daa7a5f26ca1821b87299169b003baf2d1c18
Romain <romain.chica@le-campus-numerique.com>
2024-09-04 12:04:24 +0200

    Exercice 4

--- exo-4/script.js
+++ exo-4/script.js
@@ -3,3 +3,12 @@
 console.log('exo-4');
 
 var secretMessage = ["Learning", "isn't", "about", "what", "you", "get", "easily", "the", "first", "time,", "it's", "about", "what", "you", "can", "figure", "out.", "-2015,", "Chris", "Pine,", "Learn", "JavaScript"];
+
+secretMessage.pop();
+secretMessage.push('to', 'program');
+secretMessage[6] = 'right';
+secretMessage.shift();
+secretMessage.unshift('Programming');
+secretMessage.splice(5, 5, 'know');
+// 5 est l'index où commence 'get', et 5 est le nombre d'éléments à remplacer
+console.log(secretMessage.join(' '));
